426d05a919f4371684b8466ff5303bf9037840ef
KUma1io <SwordHan@mail.ru>
2024-08-25 22:52:03 +0300

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -1,8 +1,7 @@
-name = 'Dima'
-age = 23
-age = 24
-new_age = 25
-is_student = 5 == 5
+name = 'Name: Dima'
+age = 'Age: 24'
+new_age = 'New age: 25'
+is_student = "Is student", 5 < 10
 print(name)
 print(age)
 print(new_age)
